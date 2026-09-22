603372f1b2c92f04ed0c837c01bb2c9bee97543a
EricxLuo <ericluoo12@gmail.com>
2026-09-22 18:12:06 -0400

    Use available MediaPipe WASM version

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ function Camera({ onSetComplete }: { onSetComplete: () => void }) {
         videoRef.current.srcObject = stream;
         await videoRef.current.play();
       }
-      const vision = await FilesetResolver.forVisionTasks('https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.22/wasm');
+      const vision = await FilesetResolver.forVisionTasks('https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.35/wasm');
       landmarkerRef.current = await PoseLandmarker.createFromOptions(vision, {
         baseOptions: { modelAssetPath: 'https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task', delegate: 'CPU' },
         runningMode: 'VIDEO',
